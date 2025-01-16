499bd5f2d10e1463c04873e06e2a3e34069482ef
Vicente Zanatta <vicentezanatta03@gmail.com>
2025-01-16 16:45:46 -0300

    - implementação das funções validaPosicao() e calculaDistancia(); - Mudanças no armazenamento do input.

--- solução.c
+++ solução.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
  
 const int MAXIMO = 7;
 const int MINIMO = 0;
- 
+
+int validaPosicao(int x, int y);
+int calculaDistancia(int x, int y, int dx, int dy);
  
 int main() {
  
     char Posicao_inicial[3];
     char Destino[3];
 
+    int posicaoCavalo[1][1];
+    int destino[1][1];
     int xCavalo = 0;
     int yCavalo = 0;
     int xDestino = 0;
@@ -25,5 +30,20 @@ int main() {
     yDestino = Destino[1] - 96;         // om base na tabela ASCII
     xDestino = Destino[2] - 47;
 
+    posicaoCavalo[xCavalo][yCavalo];
+    destino[xDestino][yDestino];
+
     return 0;
 }
+
+int validaPosicao(int x, int y){
+
+    return(( x > 0 && x < 7 ) && ( y > 0 && y < 7 ));
+
+}
+
+int calculaDistancia(int x, int y, int dx, int dy){
+                                                    // calcula o mudola da distancia entre uma 
+    return (((x - dx)^2 + (y - dy)^2)^(1/2));       // posicao (x,y) e o destino (dx,dy)
+
+}                                               
